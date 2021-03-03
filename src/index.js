76703364ed7ecb2c7c668394769eb6bef03e0561
Yauheni Kashlei <one.eugene.pacific@gmail.com>
2021-03-03 18:42:19 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  let l = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < l) l = array[i];
+  }
+  return l;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  let l = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > l) l = array[i];
+  }
+  return l;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  let s = 0;
+  for (let i = 0; i < array.length; i++) {
+    s += array[i];
+  }
+  s /= array.length;
+  return s;
 }
